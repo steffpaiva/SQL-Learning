af7387bc72e676f72296dd4d96da64f4c121c778
steffpaiva <stefanydepaiva@gmail.com>
2025-10-26 17:49:01 +0000

    completed exercises

--- Chapter 3/03_05b.sql
+++ Chapter 3/03_05b.sql
@@ -1,1 +1,22 @@
 -- Calculate repeat customer rate
+-- repeat customers/ total customers
+-- CTE: Virtual table created and used within query
+-- deleted after query executes
+-- used by adding WITH
+-- Add before SELECT, INSERT etc. 
+
+WITH Repeat_Customers AS
+(
+  SELECT
+  CustomerID as Repeat_Cus
+  FROM Orders
+  GROUP BY CustomerID
+  HAVING COUNT(OrderID) >1
+  )
+SELECT
+(COUNT(DISTINCT Repeat_Cus)/
+COUNT(DISTINCT CustomerID))*100
+as CustomerRepeatRate
+FROM Orders
+LEFT OUTER JOIN Repeat_Customers
+ON Orders.CustomerID = Repeat_Customers.Repeat_Cus;
--- Chapter 3/03_06_challenge.sql
+++ Chapter 3/03_06_challenge.sql
@@ -1,1 +1,15 @@
 -- Determine new customers
+-- orders, customer table
+-- first name, last number, number of orders =0,
+-- customer left join orders 
+
+SELECT 
+FirstName,
+LastName,
+COUNT(DISTINCT OrderID) as Num_Orders
+FROM Customer
+LEFT OUTER JOIN Orders ON 
+Customer.CustomerID = Orders.CustomerID
+GROUP BY Customer.CustomerID
+HAVING COUNT(OrderID) = 0;
+
